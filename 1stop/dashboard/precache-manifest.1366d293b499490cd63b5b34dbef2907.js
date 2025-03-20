26718cb2a0b515dfddd9e3330af0908d8184dd72
GeomindAI <rami@geomindai.com>
2025-03-19 22:31:51 -0400

    Reorganize to use /1stop/dashboard/ and /1stop/projections/ paths

---
+++ 1stop/dashboard/precache-manifest.1366d293b499490cd63b5b34dbef2907.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00325f2aa116f461c8f769483f97ce82",
+    "url": "/1StopDashboard/index.html"
+  },
+  {
+    "revision": "4737b58b5daafe5a25fb",
+    "url": "/1StopDashboard/static/js/2.3225a022.chunk.js"
+  },
+  {
+    "revision": "89335e2c225feb083da5",
+    "url": "/1StopDashboard/static/js/3.5b695df6.chunk.js"
+  },
+  {
+    "revision": "a938de2d4a64233cd70c",
+    "url": "/1StopDashboard/static/js/main.ea5d66ed.chunk.js"
+  },
+  {
+    "revision": "dd340db54e8b8216f63f",
+    "url": "/1StopDashboard/static/js/runtime~main.f2948074.js"
+  }
+]);
